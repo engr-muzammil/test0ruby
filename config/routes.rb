cc6f87d8f64b605d24a7e2a655f4c68ac2633946
Muzammil Ahmed <muzammil.ahmed@10pearls.com>
2022-04-01 19:37:53 +0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root "pictures#index"
+  devise_for :users
   resources :pictures
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pictures#index"
+  
 end
